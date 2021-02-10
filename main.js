5dbcea95faccd67eb49a5d24ac99c6640cc06917
Michael Peer <michaelpeer2004@gmail.com>
2021-02-10 14:49:20 +0200

    drag and drop

--- main.js
+++ main.js
@@ -1,13 +1,87 @@
 'use strict'
 
-var gElCanvas;
-var gCtx;
+let gElCanvas;
+let gCtx;
+const gTouchEvs = ['touchstart', 'touchmove', 'touchend']
+
+let gStartPos
+
 
 function onInit() {
     gElCanvas = document.getElementById('canvas')
     gCtx = gElCanvas.getContext('2d')
+    addListeners()
 
     renderGallery()
+}
+
+function addListeners() {
+    addMouseListeners()
+    // addTouchListeners()
+    // window.addEventListener('resize', () => {
+    //     resizeCanvas()
+    //     renderCanvas()
+    // })
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove)
+
+    gElCanvas.addEventListener('mousedown', onDown)
+
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove)
+
+    gElCanvas.addEventListener('touchstart', onDown)
+
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown(ev) {
+    const pos = getEvPos(ev)
+    if (!getClickedLine(pos, gElCanvas.width)) return
+    setIsDragging(true)
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+}
+
+function onMove(ev) {
+    console.log("isDragging", getMeme().isDragging)
+    if (getMeme().isDragging) {
+        const pos = getEvPos(ev)
+        const dx = pos.offsetX - gStartPos.offsetX
+        const dy = pos.offsetY - gStartPos.offsetY
+
+        setLinePos(dx, dy)
+
+        gStartPos = pos
+        renderCanvas()
+        // renderCircle()
+    }
+}
+
+function onUp() {
+    setIsDragging(false)
+    document.body.style.cursor = 'grab'
+}
+
+function getEvPos(ev) {
+    var pos = {
+        offsetX: ev.offsetX,
+        offsetY: ev.offsetY
+    }
+    if (gTouchEvs.includes(ev.type)) {
+        ev.preventDefault()
+        ev = ev.changedTouches[0]
+        pos = {
+            offsetX: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            offsetY: ev.pageY - ev.target.offsetTop - ev.target.clientTop
+        }
+    }
+    return pos
 }
 
 function renderGallery() {
@@ -48,26 +122,23 @@ function renderCanvas() {
 function drawText() {
     const meme = getMeme()
 
-    meme.lines.forEach((line) => {
+    meme.lines.forEach((line, idx) => {
 
-
-
-        if(line.txt) {
+        if (idx === meme.selectedLineIdx && line.txt) {
             gCtx.textBaseline = 'top'
             const width = gCtx.measureText(line.txt).width
             gCtx.fillRect(10, line.pos.y, gElCanvas.width - 20, parseInt(line.size, 10));
         }
 
-
+        console.log("Drawing text..")
         gCtx.lineWidth = 2
-        gCtx.strokeStyle = 'black'
+        gCtx.strokeStyle = line.color
         gCtx.fillStyle = 'white'
         gCtx.font = `${line.size}px IMPACT`
-        gCtx.textAlign = 'center'
+        gCtx.textAlign = line.align
         gCtx.fillText(line.txt, line.pos.x, line.pos.y)
         gCtx.strokeText(line.txt, line.pos.x, line.pos.y)
 
-   
     })
 
 
@@ -90,8 +161,6 @@ function drawText() {
 
 function onMemeTextChanged(elMemeText) {
     const txt = elMemeText.value
-    console.log("Text", txt)
-    if (!txt) return
     setMemeText(txt)
     renderCanvas()
     // drawText()
@@ -104,23 +173,62 @@ function onChangeFontSizeClicked(diff) {
 
 
 function onNextLineClicked() {
+    const meme = getMeme()
+    if (!meme.lines.length > 1) return
     setNextLine()
-    document.getElementById('meme-text').value = ''
+    const memeText = document.getElementById('meme-text')
+    memeText.value = meme.lines[meme.selectedLineIdx].txt
+}
+
+function resetText() {
+    const memeText = document.getElementById('meme-text')
+    memeText.value = ''
 }
 
 function onColorChanged(elColor) {
     const color = elColor.value
     if (!color) return
     setColor(color)
+    renderCanvas()
 }
 
 function onAligmentClicked(alignmentCode) {
     setAlignment(alignmentCode)
+    renderCanvas()
 }
 
 
 function onCanvasClicked(ev) {
-    const {offsetX, offsetY} = ev
-    const clickedText = getClickedText({offsetX, offsetY})
-    console.log(clickedText)
+    const { offsetX, offsetY } = ev
+    const clickedLine = getClickedLine({ offsetX, offsetY }, gElCanvas.width)
+    if (!clickedLine) return
+    renderCanvas()
+
+    const memeText = document.getElementById('meme-text')
+    const meme = getMeme()
+    memeText.value = meme.lines[meme.selectedLineIdx].txt
+    document.getElementById('meme-text').focus()
+}
+
+function onDeleteLineClicked() {
+    deleteLine()
+    renderCanvas()
+}
+
+function onAddNewLine() {
+    const meme = getMeme()
+    const memeLength = meme.lines.length
+    document.getElementById('meme-text').focus()
+    if (meme.lines[memeLength - 1].txt === '') return //can't go to other lines before the previous one filled with text
+    let pos
+    if (!memeLength) pos = { x: 225, y: 50 }
+    else if (memeLength === 1) pos = { x: 225, y: 450 }
+    else pos = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
+    console.log(pos, "pos")
+    createLine(pos)
+    resetText()
+}
+
+function onSaveClicked() {
+    saveMeme()
 }
--- services/memeService.js
+++ services/memeService.js
@@ -1,48 +1,96 @@
 'use strict'
 
+const KEY = 'memes';
+
+
 let gKeywords = { 'happy': 12, 'funny puk': 1 }
-let gImgs = [{ id: 1, url: './img/1.jpg', keywords: ['happy'] }, { id: 2, url: './img/2.jpg', keywords: ['happy'] }];
+let gImgs = [{ id: 1, url: './img/1.jpg', keywords: ['happy'] }, { id: 2, url: './img/2.jpg', keywords: ['happy'] },
+{ id: 3, url: './img/3.jpg', keywords: ['happy'] }, { id: 4, url: './img/4.jpg', keywords: ['happy'] },
+{ id: 5, url: './img/5.jpg', keywords: ['happy'] }, { id: 6, url: './img/6.jpg', keywords: ['happy'] },
+{ id: 7, url: './img/7.jpg', keywords: ['happy'] }, { id: 8, url: './img/8.jpg', keywords: ['happy'] },
+{ id: 9, url: './img/9.jpg', keywords: ['happy'] }, { id: 10, url: './img/10.jpg', keywords: ['happy'] },
+{ id: 11, url: './img/11.jpg', keywords: ['happy'] }, { id: 12, url: './img/12.jpg', keywords: ['happy'] },
+{ id: 13, url: './img/13.jpg', keywords: ['happy'] }, { id: 14, url: './img/14.jpg', keywords: ['happy'] },
+{ id: 15, url: './img/15.jpg', keywords: ['happy'] }, { id: 16, url: './img/16.jpg', keywords: ['happy'] },
+{ id: 17, url: './img/17.jpg', keywords: ['happy'] }, { id: 18, url: './img/18.jpg', keywords: ['happy'] },
+];
 
 let gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
+    isDragging: false,
     lines: [
         {
             txt: '',
             size: 30,
-            align: 'left',
-            color: 'red',
+            align: 'right',
+            color: 'black',
             pos: { x: 225, y: 50 },
         },
-        {
-            txt: '',
-            size: 30,
-            align: 'left',
-            color: 'red',
-            pos: { x: 225, y: 450 }
-        }
+        // {
+        //     txt: '',
+        //     size: 30,
+        //     align: 'right',
+        //     color: 'green',
+        //     pos: { x: 225, y: 450 }
+        // }
     ]
+}
+
+function setIsDragging(isDragging) {
+    gMeme.isDragging = isDragging
+}
+
+function setLinePos(dx, dy) {
+
+    gMeme.lines[gMeme.selectedLineIdx].pos.x += dx
+    gMeme.lines[gMeme.selectedLineIdx].pos.y += dy
+}
+
+function createLine(pos) {
+    const line = {
+        txt: '',
+        size: 30,
+        color: 'black',
+        pos
+    }
+    console.log(line, "lone")
+    gMeme.lines.push(line)
+    gMeme.selectedLineIdx = gMeme.lines.length - 1
+}
+
+function deleteLine() {
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines.splice(lineIdx, 1)
 }
 
 
 
-function getClickedText(clickedPos) {
-    // console.log(clickedPos.offsetX, clickedPos.offsety)
-    // gCtx.fillRect(10, line.pos.y, gElCanvas.width - 20, parseInt(line.size, 10));
-
-    const clickedLine = gMeme.lines.find((line) => {
-        return clickedPos.offsetX > 10 
-        && clickedPos.offsetX < 10 + gElCanvas.width - 20
-        && clickedPos.offsetY > line.pos.y
-        && clickedPos.offsetY < line.pos.y + line.size
+function getClickedLine(clickedPos, canvasWidth) {
+    // console.log("clickedPos - clickedPos", clickedPos)
+    let lineIdx
+    const clickedLine = gMeme.lines.find((line, idx) => {
+        lineIdx = idx
+        return clickedPos.offsetX > 10
+            && clickedPos.offsetX < 10 + canvasWidth - 20
+            && clickedPos.offsetY > line.pos.y
+            && clickedPos.offsetY < line.pos.y + line.size
     })
+
+    if (clickedLine) {
+        gMeme.selectedLineIdx = lineIdx
+    }
+
+    console.log(clickedLine, "clicked line")
 
     return clickedLine
 }
 
 function setNextLine() {
-    const selectedLine = gMeme.selectedLineIdx === 1 ? 0 : 1
+    const selectedLine = gMeme.selectedLineIdx === gMeme.lines.length - 1 ? 0 : gMeme.selectedLineIdx + 1
+    console.log(selectedLine, "selectedLine")
     gMeme.selectedLineIdx = selectedLine
+    renderCanvas()
 }
 
 function setMemeImgId(imgId) {
@@ -80,4 +128,14 @@ function setFontSize(diff) {
 
 function setAlignment(alignmentCode) {
     gMeme.lines[gMeme.selectedLineIdx].align = alignmentCode
+}
+
+function saveMeme() {
+    const memes = loadFromStorage(KEY) || []
+    memes.push(gMeme)
+    _saveMemeToStorage(memes)
+}
+
+function _saveMemeToStorage(memes) {
+    saveToStorage(KEY, memes)
 }
